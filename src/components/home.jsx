872797763a9670fd363d0e39f9e6b0230e6f41c0
Todd Mickel <todd@toddmickel.com>
2021-06-08 14:35:06 -0600

    Improved front page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -12,20 +12,25 @@ class Home extends Component {
   render() {
     return (
       <React.Fragment>
-        <div className="container">
+        <div className="container home-pic">
+          <img src="/img/others/ADCockpit.jpg" alt="AeroDynamica Cockpit" />
+        </div>
+        <div className="container main-text logo">
           <div className="jumbotron">
             <h1 className="display-4">Welcome to AeroDynamica!</h1>
-            <p className="lead home-text">Here at Aerodynamica, we have been serving the aviation needs of the flying public for over 50 years. Our customers have included the U.S. Government, the largest airlines in the industry, small charter operations, and individual hobbyists. Regardless of the size of your operation, you can always count on getting personal, friendly service and the best prices in the industry.</p>
+            <p className="lead home-text">Here at AeroDynamica, we have been serving the aviation needs of the flying public for over 50 years. Our customers have included the U.S. Government, the largest airlines in the industry, small charter operations, and individual hobbyists. Regardless of the size of your operation, you can always count on getting personal, friendly service and the best prices in the industry.</p>
             <p className="lead home-text">In addition to our extensive catalog of avionics components available on our website, our deep ties throughout the aviation industry allow us to quickly find low-cost, high-quality parts of any type.</p>
             <hr className="my-4" />
-            <p>Click below to see our catalog of high-quality, competitively-priced, American made products</p>
-            <p className="lead">
-              <Link className="btn btn-info btn-sm" to="/catalog" role="button">
-                Catalog <FontAwesomeIcon icon={faChevronCircleRight} />
-              </Link>
-            </p>
+            <p className="lead">Click below to see our catalog of high-quality, competitively-priced, American made products</p>
+          </div>
+
+          <div className="main-bkgnd">
+            <img src="/img/logo/propeller.png" alt="" className="App-logo" />
           </div>
         </div>
+        <Link className="btn btn-info btn-sm cat-btn" to="/catalog" role="button">
+          Catalog <FontAwesomeIcon icon={faChevronCircleRight} />
+        </Link>
         <div className="container">
           <div className="slideshow">
             <Slide easing="ease" pauseOnHover={false} arrows={false} duration={3000}>
@@ -49,9 +54,6 @@ class Home extends Component {
               </div>
             </Slide>
           </div>
-        </div>
-        <div className="logo">
-          <img src="/img/logo/propeller.jpg" alt="logo" className="App-logo"></img>
         </div>
       </React.Fragment>
     );
